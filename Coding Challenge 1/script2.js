aa9ac317e809af8888b101389e8a6699536fbc09
My Name <myEmail@example.com>
2020-05-07 18:04:56 +0300

    coding challenge

---
+++ Coding Challenge 1/script2.js
@@ -0,0 +1,30 @@
+(function($) {
+	var s,
+	spanizeLetters = {
+	  settings: {
+		letters: $('.js-spanize'),
+	  },
+	  init: function() {
+		s = this.settings;
+		this.bindEvents();
+	  },
+	  bindEvents: function(){
+		s.letters.html(function (i, el) {
+		  //spanizeLetters.joinChars();
+		  var spanizer = $.trim(el).split("");
+		  return '<span>' + spanizer.join('</span><span>') + '</span>';
+		});
+	  },
+	};
+	spanizeLetters.init();
+  })(jQuery);
+
+  function testResults (form) {
+	var TestVar = form.inputbox.value;
+	console.log(TestVar)
+	if (TestVar == "421") {
+		window.location.href = "pageS.html";
+	}else{
+		window.location.href = "pageF.html";
+	}
+}
--- Week2/Day 5/Mini Project xp calculator/script.js
+++ Week2/Day 5/Mini Project xp calculator/script.js
@@ -9,46 +9,55 @@ console.log(arr,'arr');
 
 
 function calcul(){
+    let sum = ''
+    let sum2 = ''
+    let r = 0
 
-  
-     
-    sum = ''
-        for(i=0; arr[i] != ('+'||'-'||'*'|| '%'); i++){ 
+    for(i=0; arr[i] != ('+'||'-'||'*'|| '/'); i++){ 
 
-            sum = sum + arr[i];
-            i2 = i;
-            console.log(sum,i2 ,'sum');
+        sum += arr[i];
+        i2 = i+2;
+        console.log("sum " + sum);      
+    }   
+    if(arr[i]='+'||'-'||'*'|| '/'){
+
+        for (i=i2; i < arr.length; i++){
         
+        sum2 += arr[i]
+        console.log("sum2 " + sum2)
         }
-
-        // for(i=i2+1; arr[i] ; i++){
-
-        //     let sum2 = sum2 + num[i];
-        // }
-        
-        // if(num[i2] = '+'){
-
-        //     let r = sum + sum2;
-
-        // }else if (num[i2] = '-'){
-
-        //     r = sum - sum2;
-
-        // }else if (num[i2] = '*'){
-
-        //     r = sum * sum2;
-
-        // }else {
-
-        //     r = sum % sum2;
-        // }
-
-        //console.log(r);
+    }  
     
+    
+    if(arr[i] = '+'){
+
+        r = parseInt(sum) + parseInt(sum2);
+        console.log("sum " + sum)
+        console.log("sum2 " + sum2)
+
+
+    }else if (arr[i] = '-'){
+
+        r = parseInt(sum) - parseInt(sum2);
+        console.log("sum " + sum)
+        console.log("sum2 " + sum2)
+
+    }else if (arr[i] = '*'){
+
+        r = parseInt(sum) * parseInt(sum2);
+        console.log("sum " + sum)
+        console.log("sum2 " + sum2)
+
+    }else{
+
+        r = parseInt(sum) / parseInt(sum2);
+        console.log("sum " + sum)
+        console.log("sum2 " + sum2)
+    }
+
+    console.log(r);
 
 }
-
-
 
 
 
